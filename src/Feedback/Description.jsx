438953e742426cdd36ba29307cf11aff7dd873b9
RuslanKabachok <ruslan.kabachok@gmail.com>
2024-02-14 19:10:43 +0200

    added description

---
+++ src/Feedback/Description.jsx
@@ -0,0 +1,12 @@
+import css from '../Feedback/Description.module.css';
+
+function Description({ title, text }) {
+  return (
+    <>
+      <h1 className={css.title}>{title}</h1>
+      <p className={css.text}>{text}</p>
+    </>
+  );
+}
+
+export default Description;
---
+++ src/Feedback/Feedback.jsx
@@ -0,0 +1,16 @@
+import Description from './Description';
+
+function Feedback() {
+  return (
+    <>
+      <div>
+        <Description
+          title="Sip Happens Café"
+          text="Please leave your feedback about our service by selecting one of the options below."
+        />
+      </div>
+    </>
+  );
+}
+
+export default Feedback;
